d99759a724e52f3672cacfac1eaa738a29583ad1
201311249/daeyoung <rvd619rey@naver.com>
2016-10-18 13:16:12 +0900

    Delete 201311249_drawSquareAt.java

--- 201311249_drawSquareAt.java
+++
@@ -1,9 +0,0 @@
-import ch.aplu.turtle.*;
-void drawSquareAt(int size, double x, double y){
-for(int i=0; i<4; i++){
-t1.forward(x);
-t1.right(y);
-}
-}
-Turtle t1= new Turtle();
-drawSquareAt(50,100,90);
